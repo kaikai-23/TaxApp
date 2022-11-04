9f0487c487d9a3db15d8b4a0d2352873b1fbc313
kaikai-23 <gate.sea0203@gmail.com>
2022-11-04 22:02:30 +0900

    Initial Commit

---
+++ TaxApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaxApp
+//
+//  Created by 寳門海 on 2022/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
